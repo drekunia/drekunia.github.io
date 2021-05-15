c1eadc435b62cee454a775fddfc6828ef989ed27
Andre Kurniawan <drekunia@gmail.com>
2021-05-16 00:24:08 +0700

    error

--- js/contactEmailAjax.js
+++ js/contactEmailAjax.js
@@ -10,18 +10,18 @@ function handleSubmit() {
     headers: {
       Accept: 'application/json',
     },
-    success: function () {
-      status.innerHTML = 'Sent!';
-      Swal.fire('Thank you!', 'Thanks for your submission.', 'success');
-      form.reset();
-    },
-    error: function () {
+  }).then((response) => {
+    if (response.status !== 200) {
       status.innerHTML = 'An error has occurred.';
       Swal.fire(
         'Oops...',
-        'There was a problem submitting your form.<br>Just email to drekunia@gmail.com from your email client.',
+        'There was a problem submitting your form.',
         'error'
       );
-    },
+    } else {
+      status.innerHTML = 'Sent!';
+      Swal.fire('Thank you!', 'Thanks for your submission.', 'success');
+      form.reset();
+    }
   });
 }
