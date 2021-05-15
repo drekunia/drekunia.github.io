bb73bfcc29a1392bcb5685ff5b2c9c86ed6b84e3
Andre Kurniawan <drekunia@gmail.com>
2021-05-16 00:27:33 +0700

    error

--- js/contactEmailAjax.js
+++ js/contactEmailAjax.js
@@ -15,7 +15,7 @@ function handleSubmit() {
       status.innerHTML = 'An error has occurred.';
       Swal.fire(
         'Oops...',
-        'There was a problem submitting your form.',
+        "There was a problem submitting your form.<br>Please send your email to <a href='mailto:drekunia@gmail.com'>drekunia@gmail.com</a>.",
         'error'
       );
     } else {
